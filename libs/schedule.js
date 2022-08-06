0ce8890753850c0e2b53c1a1c8f7c7f4a8315393
brick9527 <brick9527@foxmail.com>
2022-08-06 23:59:27 +0800

    fix: 修复用户不传入间隔时间时报错问题

--- libs/schedule.js
+++ libs/schedule.js
@@ -4,6 +4,7 @@ const getAllPerformance = require('./get_all_performance');
 const sendUDP = require('./udp');
 const print = require('./print');
 const appendFile = require('./apend_file');
+const config = require('../config.json');
 
 function _getAllPerformanceAndSend(argvs) {
   return async function () {
@@ -16,7 +17,7 @@ function _getAllPerformanceAndSend(argvs) {
 
 module.exports = function(argvs) {
   const stat = new CronJob(
-    `* */${argvs.interval} * * *`,
+    `* */${argvs.interval || config.interval} * * *`,
     _getAllPerformanceAndSend(argvs),
     null,
     null,
